07659a53073d5799f40f88756d124103dbc3b3de
mozombick <mozombick@DESKTOP-D1Q0SAR>
2024-01-15 19:27:19 +0300

    Добавьте файлы проекта.

---
+++ calculator C.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "calculator.h"
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper;
+
+
+    cout << "calc console application" << endl << endl;
+    cout << "enter the operation in format: a+b a-b a*b a/b \n"
+    << endl;
+
+    calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "DOLBOEB\n" << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+
+        cout << "Result" << " of " << x << oper << y << " is " << result << endl;
+    }
+        
+    
+    return 0;
+}
